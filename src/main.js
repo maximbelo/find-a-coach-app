62b5bd19db75b2dca5ea7790c3c2b4390018088c
maximbelo <mb@tineye.com>
2022-06-21 16:42:22 -0400

    Added base components and styling

--- src/main.js
+++ src/main.js
@@ -3,9 +3,18 @@ import store from "./store/index.js";
 import App from "./App.vue";
 import router from "./router.js";
 
+// Base Components
+import BaseCard from "./components/ui/BaseCard.vue";
+import BaseButton from "./components/ui/BaseButton.vue";
+import BaseBadge from "./components/ui/BaseBadge.vue";
+
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
+
+app.component("base-card", BaseCard);
+app.component("base-button", BaseButton);
+app.component("base-badge", BaseBadge);
 
 app.mount("#app");
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,26 +1,27 @@
 <template>
   <section>FILTER</section>
+  <base-card>
+    <section>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register as Coach</base-button>
+      </div>
 
-  <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
-    </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        ></coach-item>
+      </ul>
 
-    <ul v-if="hasCoaches">
-      <coach-item
-        v-for="coach in filteredCoaches"
-        :key="coach.id"
-        :id="coach.id"
-        :first-name="coach.firstName"
-        :last-name="coach.lastName"
-        :rate="coach.hourlyRate"
-        :areas="coach.areas"
-      ></coach-item>
-    </ul>
-
-    <h3 v-else>No Coaches Found!</h3>
-  </section>
+      <h3 v-else>No Coaches Found!</h3>
+    </section>
+  </base-card>
 </template>
 
 <script>
