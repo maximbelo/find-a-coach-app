421320c0fb64de8885249c2f225d597eadd9c2e1
maximbelo <mb@tineye.com>
2022-06-21 10:20:29 -0400

    Setup router and main.js and added components to the app

---
+++ src/router.js
@@ -0,0 +1,47 @@
+import { createRouter, createWebHistory } from "vue-router";
+import CoachDetail from "./pages/coaches/CoachDetail.vue";
+import CoachesList from "./pages/coaches/CoachesList.vue";
+import CoachRegistration from "./pages/coaches/CoachRegistration.vue";
+import ContactCoach from "./pages/requests/ContactCoach.vue";
+import RequestsReceived from "./pages/requests/RequestsReceived.vue";
+import NotFound from "./pages/NotFound.vue";
+
+const routes = [
+  {
+    path: "/",
+    redirect: "/coaches",
+  },
+  {
+    path: "/coaches",
+    component: CoachesList,
+  },
+  {
+    path: "/coaches:id",
+    component: CoachDetail,
+    children: [
+      {
+        path: "contact", // coaches/c1/contact
+        component: ContactCoach,
+      },
+    ],
+  },
+  {
+    path: "/register",
+    component: CoachRegistration,
+  },
+  {
+    path: "/requests",
+    component: RequestsReceived,
+  },
+  {
+    path: "/:notFound(.*)",
+    component: NotFound,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
