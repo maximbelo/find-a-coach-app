e4a869e7d615ee74e8c2ab158a426f95fb1ef461
maximbelo <mb@tineye.com>
2022-06-27 13:18:22 -0400

    Added Coach Form

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,1 +1,13 @@
-<template>REGISTER</template>
+<template>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import CoachForm from "../../components/coaches/CoachForm.vue";
+export default { components: { CoachForm } };
+</script>
