e905729894519d3b61af9bc06cab97dd53d4df44
maximbelo <mb@tineye.com>
2022-07-27 15:39:20 -0400

    Added login action and flow on the Auth component

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -28,5 +28,32 @@ export default {
     });
   },
 
-  login() {},
+  async login(context, payload) {
+    const response = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.VUE_APP_NOT_SECRET_KEY}`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    const responseData = response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || "Failed to authenticate. Please try again.");
+      console.log(responseData);
+      throw error;
+    }
+
+    console.log(responseData);
+    context.commit("setUser", {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
 };
